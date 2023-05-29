4e218de646fffce35632843e08069005ce971f93
John Rashin <lomoyajohnrashin@gmail.com>
2023-05-29 22:22:38 +0800

    final commit

--- app/src/main/java/com/example/bisuinformationapp/creaturesMenu.java
+++ app/src/main/java/com/example/bisuinformationapp/creaturesMenu.java
@@ -1,17 +1,23 @@
 package com.example.bisuinformationapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -21,20 +27,28 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.UUID;
 
 public class creaturesMenu extends AppCompatActivity {
 
     private static final int FILE_PICKER_REQUEST_CODE = 1;
-    FirebaseDatabase admin = FirebaseDatabase.getInstance();
-
+    TextView fileTextView;
     EditText datePicker;
     EditText eventNameTxt,dateTxt,locationTxt,descriptionTxt;
     Button submit;
+    DatabaseReference myRef;
+    Uri selectedFileUri;
+    StorageReference storageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,22 +100,20 @@ public class creaturesMenu extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
-
         //attach file function
-        Button uploadButton = findViewById(R.id.uploadButton);
-        uploadButton.setOnClickListener(new View.OnClickListener() {
+        fileTextView = findViewById(R.id.fileTextView);
+        fileTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("*/*");  // Set the MIME type or restrict to specific file types (e.g., "image/*" for images)
+                intent.setType("*/*");
                 startActivityForResult(intent, FILE_PICKER_REQUEST_CODE);
-
             }
         });
-// Submitting events into apps
+
+         // Submitting events into apps
         submit = findViewById(R.id.submitBTN);
         submit.setOnClickListener(new View.OnClickListener() {
-            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("events");
 
             @Override
             public void onClick(View v) {
@@ -110,9 +122,14 @@ public class creaturesMenu extends AppCompatActivity {
                 String location = locationTxt.getText().toString();
                 String description = descriptionTxt.getText().toString();
 
+                myRef = FirebaseDatabase.getInstance().getReference("events");
+                storageRef = FirebaseStorage.getInstance().getReference();
                 // Write the data to the database
-                admin event = new admin("",eventName, date, location, description);
-                myRef.push().setValue(event)
+                admin event = new admin("",eventName, date, location, description,"");
+                DatabaseReference newEventRef = myRef.push();
+                String eventId = newEventRef.getKey(); // Get the key of the newly created event
+                event.setEventID(eventId);
+                newEventRef.setValue(event)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
@@ -122,29 +139,78 @@ public class creaturesMenu extends AppCompatActivity {
                                 locationTxt.getText().clear();
                                 descriptionTxt.getText().clear();
                                 Toast.makeText(creaturesMenu.this, "Data inserted successfully", Toast.LENGTH_SHORT).show();
+                                if (selectedFileUri != null) {
+                                    String filePath = "document/files/";
+                                    String fileId = UUID.randomUUID().toString(); // Generate a unique ID for the file
+                                    String fileStoragePath = filePath + fileId; // Append the unique ID to the storage path
+
+                                    UploadTask uploadTask = storageRef.child(fileStoragePath).putFile(selectedFileUri);
+                                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                            storageRef.child(fileStoragePath).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                @Override
+                                                public void onSuccess(Uri downloadUrl) {
+                                                    String downloadUrlString = downloadUrl.toString();
+                                                    Toast.makeText(creaturesMenu.this, "File uploaded successfully", Toast.LENGTH_SHORT).show();
+                                                    updateFileDownloadUrl(eventId, downloadUrlString);
+                                                }
+                                            })
+                                            ;
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(creaturesMenu.this, "File upload failed", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                // Failed to write data to the database
                                 Toast.makeText(creaturesMenu.this, "Failed to insert data", Toast.LENGTH_SHORT).show();
                             }
                         });
             }
         });
+
     }
-        @Override
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
-            // Get the URI of the selected file
-            Uri fileUri = data.getData();
-
-            // Perform further operations with the file
-            // For example, you can upload it to a server using networking libraries or save it locally.
+            if (data != null && data.getData() != null) {
+                selectedFileUri = data.getData();
+                String fileName = getFileName(selectedFileUri);
+                fileTextView.setText(fileName);
+            }
         }
     }
 
+    // Utility method to get the file name from the URI
+    private String getFileName(Uri uri) {
+        String fileName = "";
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                if (nameIndex != -1) {
+                    fileName = cursor.getString(nameIndex);
+                }
+                cursor.close();
+            }
+        } else if (uri.getScheme().equals("file")) {
+            fileName = new File(uri.getPath()).getName();
+        }
+        return fileName;
+    }
+    // Update the download URL of the file in the Realtime Database
+    private void updateFileDownloadUrl(String eventId, String downloadUrl) {
+        DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("events").child(eventId).child("document");
+        eventRef.setValue(downloadUrl);
+}
 }
--- app/src/main/java/com/example/bisuinformationapp/dashboard.java
+++ app/src/main/java/com/example/bisuinformationapp/dashboard.java
@@ -6,16 +6,20 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class dashboard extends AppCompatActivity {
-    CardView onClickEvent, viewEvent;
-
+    CardView onClickEvent, viewEvent,calendar;
+    Button logOutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         onClickEvent = findViewById(R.id.createEvent);
         viewEvent = findViewById(R.id.viewEvent);
+        logOutBtn = findViewById(R.id.logOutBtn);
+        calendar=findViewById(R.id.calendarIn);
 
         onClickEvent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +34,23 @@ public class dashboard extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(dashboard.this, infoDashboard.class);
                 startActivity(intent);
+            }
+        });
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, calendar.class);
+                startActivity(intent);
+            }
+        });
+
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, logInscreen.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(dashboard.this,"Logout Successful!",Toast.LENGTH_SHORT).show();
             }
         });
     }
